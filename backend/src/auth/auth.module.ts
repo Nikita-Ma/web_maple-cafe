fc4a8d14063cdd00b2655e837c96b680fc020e36
Nik <nikitama1hr@gmail.com>
2023-09-12 01:03:23 +0300

    create DRAFT & VERY BAD middleware auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
@@ -7,6 +12,8 @@ import { jwtConstants } from './constants';
 import { Users } from '../users/entities/users.entity';
 import { UsersService } from '../users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerMiddleware } from '../logger.middleware';
+import { AuthMiddleware } from './auth.middleware';
 
 @Module({
   imports: [
@@ -21,6 +28,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   controllers: [AuthController],
   providers: [AuthService, UsersService], // Include UsersRepository if it's a provider
 })
-export class AuthModule {
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: '/auth/*', method: RequestMethod.ALL });
+  }
 }
-
