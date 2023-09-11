86665fb76e0c320b3ffa7ef5979ede4a64a98f99
Nik <nikitama1hr@gmail.com>
2023-09-11 15:03:36 +0300

    setting docker-compose.yaml and create basic entity user.entity.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: '12345',
+      password: '12345',
+      database: '12345',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/user.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  firstName: string;
+  @Column()
+  lastName: string;
+  @Column()
+  password: string;
+  @Column({ default: 'user' })
+  status: string;
+  @Column()
+  telephone: string;
+  @Column()
+  email: string;
+  @Column({ default: false })
+  banned: boolean;
+  @Column()
+  orders: number;
+  @Column({ default: 0 })
+  restMoney: number;
+  @Column({ type: 'timestamptz' }) // Recommended
+  date_time_with_timezone: Date;
+}
